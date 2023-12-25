244f8b06a626ea1afba7dd59267287e256ee6534
KseniiaSm <143532668+KseniiaSm@users.noreply.github.com>
2023-12-25 21:13:21 +0300

    2

--- src/main/java/arrayList_Collections/ArrayListTest.java
+++ src/main/java/arrayList_Collections/ArrayListTest.java
@@ -1,11 +1,6 @@
-package arrayList;
+package arrayList_Collections;
 
-import javax.swing.text.html.HTMLDocument;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.*;
 
 /*
 необходимо импортировать пакет для использования списочного массива
@@ -36,32 +31,32 @@ public class ArrayListTest {
         /**
          * ArrayList в который можно добавлять только объекты одного типа данных
          * */
-        ArrayList <String> list2 = new ArrayList<>(30);
+        ArrayList<String> list2 = new ArrayList<>(30);
         list2.add("Privet");
         /* При этом я не могу добавить объект типа Car
-        * В скобках пишем вместимость, если необходимо
-        * Есть понятие размер (сколько там объектов)
-        * А есть понятие вместимость (максимально сколько можно поместить объектов)
-        * ArrayList <String> list2 = new ArrayList<>(30);
+         * В скобках пишем вместимость, если необходимо
+         * Есть понятие размер (сколько там объектов)
+         * А есть понятие вместимость (максимально сколько можно поместить объектов)
+         * ArrayList <String> list2 = new ArrayList<>(30);
          */
 
-        ArrayList <String> list4 = new ArrayList<>(list2);
+        ArrayList<String> list4 = new ArrayList<>(list2);
         /* Можно записать (list2)
-        * это значит, что list4 ссылается на подобный лист, но не на тот же самый (с такой же вместимостью 30 */
+         * это значит, что list4 ссылается на подобный лист, но не на тот же самый (с такой же вместимостью 30 */
 
         /** Можно записать через лист**/
         List<String> list3 = new ArrayList<>();
 
-                                         /** Методы Списочного массива **/
+        /** Методы Списочного массива **/
         /**
          1. Добавление элемента .add(int номер элемента, "String");*/
         System.out.println(1);
-        ArrayList <String> list5 = new ArrayList<>();
+        ArrayList<String> list5 = new ArrayList<>();
         list5.add("Privet");
         list5.add("Poka");
-        System.out.println("list5: "+ list5);
-        list5.add(0,"Первый");
-        System.out.println("list5: "+ list5);
+        System.out.println("list5: " + list5);
+        list5.add(0, "Первый");
+        System.out.println("list5: " + list5);
 
         /* Так делать нельзя, нельзя добавлять элементы слишком далеко (чтобы были пустые до него)
         list5.add(7,"ВосьмойЭлемент");
@@ -85,20 +80,20 @@ public class ArrayListTest {
         StringBuilder sb1 = new StringBuilder("One");
         StringBuilder sb2 = new StringBuilder("Two");
         StringBuilder sb3 = new StringBuilder("Three");
-        ArrayList <StringBuilder> sb = new ArrayList<>();
+        ArrayList<StringBuilder> sb = new ArrayList<>();
         sb.add(sb1);
         sb.add(sb2);
         sb.add(sb3);
         System.out.println(sb);
         /*        Вызвать размер листа с помощью метода size()         */
-        for (int i=0;i<list5.size();i++){
+        for (int i = 0; i < list5.size(); i++) {
             sb.get(i).append("!!!");
         }
         System.out.println(sb);
 
         /**
          4. Удалить элемент .remove("Содержимое элемента (напр. стринг")
-           или              .remove(int номер элемента)
+         или              .remove(int номер элемента)
          */
         System.out.println();
         System.out.println(4);
@@ -110,8 +105,8 @@ public class ArrayListTest {
         System.out.println(list5 + " после удаления первого элемента Poka");
 
         /**
-        !!!!!
-        Это не работает если вписать содержимое объекта (т.е. если у нас есть стрингбилдер и мы вписываем
+         !!!!!
+         Это не работает если вписать содержимое объекта (т.е. если у нас есть стрингбилдер и мы вписываем
          в remove(его содержимое), то ничего не произвойдет
          необходимо вписать его имя!!!
          */
@@ -134,7 +129,7 @@ public class ArrayListTest {
         System.out.println(list5);
         /** Добавить не в конец, а в любое место
          * */
-        list5.addAll(0,list2);
+        list5.addAll(0, list2);
         System.out.println(list5);
 
         /**
@@ -151,7 +146,7 @@ public class ArrayListTest {
          */
         System.out.println();
         System.out.println(7);
-        ArrayList <String> fruits = new ArrayList<>();
+        ArrayList<String> fruits = new ArrayList<>();
         fruits.add("Apple");
         fruits.add("Banana");
         fruits.add("Pineapple");
@@ -164,10 +159,10 @@ public class ArrayListTest {
         System.out.println("А можно искать так: ");
         System.out.println(sb.indexOf(sb1) + " можно искать по имени объекта");
 
-         /**
-          8. Найти размер листа .size();
+        /**
+         8. Найти размер листа .size();
 
-          9. Пустой ли лист isEmpty();
+         9. Пустой ли лист isEmpty();
          */
         System.out.println();
         System.out.println(9);
@@ -181,15 +176,15 @@ public class ArrayListTest {
         System.out.println(list2.contains("k"));
 
         /**
-        11. Вывести на экран красиво .toString();
+         11. Вывести на экран красиво .toString();
          */
 
         /**
-        12. Склонировать лист в другой лист .clone(list2); (см ниже как клонировать)
+         12. Склонировать лист в другой лист .clone(list2); (см ниже как клонировать)
          */
         System.out.println();
         System.out.println(12);
-        ArrayList <StringBuilder> fruitALot = new ArrayList<>();
+        ArrayList<StringBuilder> fruitALot = new ArrayList<>();
         StringBuilder apple = new StringBuilder("Apple");
         StringBuilder banana = new StringBuilder("Banana");
         StringBuilder pineapple = new StringBuilder("Pineapple");
@@ -200,7 +195,7 @@ public class ArrayListTest {
         //ArrayList <StringBuilder> cloneFruitALot = new ArrayList<>();
 
         //Необходимо так записать (по-другому не работает)
-        ArrayList <StringBuilder> cloneFruitALot = (ArrayList <StringBuilder>)fruitALot.clone();
+        ArrayList<StringBuilder> cloneFruitALot = (ArrayList<StringBuilder>) fruitALot.clone();
         System.out.println(cloneFruitALot.get(0));
         System.out.println(cloneFruitALot.get(0) == fruitALot.get(0)); // true
         System.out.println(cloneFruitALot == fruitALot); //false
@@ -211,7 +206,7 @@ public class ArrayListTest {
          */
         System.out.println();
         System.out.println(13);
-        StringBuilder[] arrayFromFruitALot = fruitALot.toArray(new StringBuilder [3]);
+        StringBuilder[] arrayFromFruitALot = fruitALot.toArray(new StringBuilder[3]);
         System.out.println(Arrays.toString(arrayFromFruitALot));
 
         /**
@@ -219,7 +214,7 @@ public class ArrayListTest {
          */
         System.out.println();
         System.out.println(14);
-        List <StringBuilder> listTypeList = Arrays.asList(arrayFromFruitALot);
+        List<StringBuilder> listTypeList = Arrays.asList(arrayFromFruitALot);
 
         /**
          15. Отсортировать ArrayList
@@ -231,7 +226,7 @@ public class ArrayListTest {
         String s2 = "B";
         String s3 = "C";
         String s4 = "D";
-        ArrayList <String> list15 = new ArrayList<>();
+        ArrayList<String> list15 = new ArrayList<>();
         list15.add(s4);
         list15.add(s1);
         list15.add(s2);
@@ -245,27 +240,27 @@ public class ArrayListTest {
          одинАррейЛист.equals(второйАррейЛист)
          */
 
-         /**
+        /**
          17. Compare - показывает какой массив больше или меньше по алфавиту или по списку цифр
-          лексикографическое сравнение аррейЛистов
-          как в словаре: сначала идет слово
-          Привет
-          Прививка потом (по алфавиту)
-          также
-          12345
-          1235
+         лексикографическое сравнение аррейЛистов
+         как в словаре: сначала идет слово
+         Привет
+         Прививка потом (по алфавиту)
+         также
+         12345
+         1235
          */
         System.out.println();
         System.out.println(17);
-         int [] array1 = {1, 2, 3, 4, 5};
-         int [] array2 = {1, 2, 3, 5};
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 2, 3, 5};
         System.out.println(Arrays.compare(array1, array2));
         //Он вернет "-1" - если первый массив меньше второго (по алфавиту)
         //Он вернет "0" - если равны
         //Он вернет "1" - если второй массив меньше первого (по алфавиту)
 
-         char [] array3 = {'P', 'r', 'i', 'v', 'e', 't'};
-         char [] array4 = {'P', 'r', 'i', 'v', 'i', 'v', 'k', 'a'};
+        char[] array3 = {'P', 'r', 'i', 'v', 'e', 't'};
+        char[] array4 = {'P', 'r', 'i', 'v', 'i', 'v', 'k', 'a'};
         System.out.println(Arrays.compare(array3, array4));
         //-4 так как первый меньше чем второй
 
@@ -290,7 +285,7 @@ public class ArrayListTest {
 
         System.out.println();
         System.out.println("Iterators");
-        Iterator <String> it = list15.iterator();
+        Iterator<String> it = list15.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
         }
@@ -303,31 +298,12 @@ public class ArrayListTest {
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
 }
 
-class Car{}
+class Car {
+}
 
-class Student{}
+class Student {
+}
---
+++ src/main/java/arrayList_Collections/ListMethods.java
@@ -0,0 +1,126 @@
+package arrayList_Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethods {
+    /**
+     * List - это не изменяемая структура
+     * <p>
+     * Если убрать тип данных в листе (<String> напр.), то тип данных станет Object
+     * и могут возникнуть проблемы, которые не заметит компилятор
+     */
+
+
+    public static void main(String[] args) {
+        /**
+         1. Удалить все элементы в массиве, которые есть в другом массиве
+         .removeAll(ArrayList)
+         */
+        System.out.println(1);
+
+        ArrayList<String> bigArrayList = new ArrayList<>();
+        bigArrayList.add("One");
+        bigArrayList.add("Two");
+        bigArrayList.add("Three");
+        bigArrayList.add("Four");
+        bigArrayList.add("Five");
+        System.out.println(bigArrayList + " bigArrayList");
+
+        ArrayList<String> smallArrayList = new ArrayList<>();
+        smallArrayList.add("Three");
+        smallArrayList.add("Five");
+        smallArrayList.add("Ten");
+        System.out.println(smallArrayList + " smallArrayList");
+
+        bigArrayList.removeAll(smallArrayList);
+        System.out.println(bigArrayList + " bigArrayList после применения метода removeAll");
+
+        /**
+         2. Сохранить только элементы, которые есть в другом массиве
+         .retainAll(ArrayList)
+         */
+        System.out.println();
+        System.out.println(2);
+
+        ArrayList<String> bigArrayList2 = new ArrayList<>();
+        bigArrayList2.add("One");
+        bigArrayList2.add("Two");
+        bigArrayList2.add("Three");
+        bigArrayList2.add("Four");
+        bigArrayList2.add("Five");
+        System.out.println(bigArrayList2 + " bigArrayList2");
+
+        ArrayList<String> smallArrayList2 = new ArrayList<>();
+        smallArrayList2.add("Three");
+        smallArrayList2.add("Five");
+        smallArrayList2.add("Ten");
+        System.out.println(smallArrayList2 + " smallArrayList2");
+
+        bigArrayList2.retainAll(smallArrayList2);
+        System.out.println(bigArrayList2 + " bigArrayList2 после применения метода retaintAll");
+
+        /**
+         3. Содержит ли массив все элементы, которые есть в другом массиве
+         .containsAll(ArrayList)
+         */
+        System.out.println();
+        System.out.println(3);
+
+        ArrayList<String> bigArrayList3 = new ArrayList<>();
+        bigArrayList3.add("One");
+        bigArrayList3.add("Two");
+        bigArrayList3.add("Three");
+        bigArrayList3.add("Four");
+        bigArrayList3.add("Five");
+        System.out.println(bigArrayList3 + " bigArrayList3");
+
+        ArrayList<String> smallArrayList3 = new ArrayList<>();
+        smallArrayList3.add("Three");
+        smallArrayList3.add("Five");
+        smallArrayList3.add("Ten");
+        System.out.println(smallArrayList3 + " smallArrayList3");
+
+        boolean containsElements = bigArrayList3.containsAll(smallArrayList3);
+        System.out.println(containsElements + " содержит ли все элементы маленького массива?");
+
+        /**
+         4. Кусок ArrayList - но возвращает уже List !!!!!!
+         .sublist(index начала, index конца) => List
+         */
+        System.out.println();
+        System.out.println(4);
+
+        System.out.println(bigArrayList3 + " bigArrayList3");
+        List<String> sublist = bigArrayList3.subList(2, 4); // 4 - не включительно
+        System.out.println(sublist + " sublist");
+
+        /**
+         5. List !!!!!!
+         Быстро создать List
+         List.of(элементы)
+         List <String> list = List.of("One", "Two", "Three");
+         */
+        System.out.println();
+        System.out.println(5);
+
+        List<String> list = List.of("Raz", "Dva", "Tri");
+        System.out.println(list + " list");
+        //!!!Лист потом никак не изменить!!!
+
+        /**
+         6. List !!!!!!
+         Скопировать в List
+         List.copyOf(элементы)
+         */
+        System.out.println();
+        System.out.println(6);
+
+        List<String> list2 = List.copyOf(list);
+        System.out.println(list2 + " list2 скопированный");
+        //!!!Лист потом никак не изменить!!!
+
+
+    }
+
+}
